0f521b80494460bd413aef13dad3eca2b7fe0fd9
KIM_SC <kimsung01265@gmail.com>
2024-12-31 15:24:05 +0900

    우선 페이지만 추가

---
+++ pages/about_me.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>About Me</h2>
+    <p>저는 웹 개발자입니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped>
+/* 스타일 추가 */
+</style>
