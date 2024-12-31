6cb2dd46042d960164829c2e2988ac5b1e45dc1b
KIM_SC <kimsung01265@gmail.com>
2024-12-31 15:24:24 +0900

    인덱스 페이지 임시 제작 테스트 중

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,60 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+    <!-- 소개 섹션 -->
+    <section id="">
+      <div class="">안녕하세요</div>
+      <div class="">~~~~~ 하는 것을 추구합니다.</div>
+
+    </section>
+
+    <!-- 경력 및 보유 자격 사항 섹션 -->
+    <section id="career and licencs">
+      <div>About Me</div>
+      <td>
+        <div class="career container">
+          <li>훈훈소프트 2024.12.16 ~ 2025.02.14 </li>
+        </div>
+      </td>
+      <div class="licencse container">
+        <li>정보처리 산업기사</li>
+        <li>리눅스 마스터 2급</li>
+        <li>운전면허증 2종</li>
+        <li>네트워크 관리사 2급</li>
+        <li>전자기능사</li>
+      </div>
+    </section>
+
+    <!-- 스킬 섹션 -->
+    <section id="skills">
+      <div>Skills</div>
+      <div class="main-skills">
+        <div class="front-end">front-end</div>
+      </div>
+      <div class="sub-skills">
+        <p>사용해봤어요</p>
+      </div>
+    </section>
+
+    <!-- 프로젝트 섹션 -->
+    <section id="projects">
+      <div>Project</div>
+      <div class="team-project"></div>
+
+      <div class="personal-project"></div>
+    </section>
+
+    <!-- 컨택 섹션 -->
+    <section id="contact">
+      <div>Contact Me</div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+
 }
 </script>
