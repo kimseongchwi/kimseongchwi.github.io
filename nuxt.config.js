54d74501573fd6ef79839338bfc3f3fb3096038b
KIM_SC <kimsung01265@gmail.com>
2024-12-31 15:16:28 +0900

    router base를 기본 폴더로 지정

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Your portfolio description here' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
@@ -37,5 +37,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  router: {
+    base: '/'
   }
 }
